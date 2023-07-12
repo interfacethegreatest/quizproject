4242e10c94b30998d41973af655cd89d537acf4c
Drew Ellington <32873719+interfacethegreatest@users.noreply.github.com>
2023-07-12 16:04:30 +0100

    Add files via upload

--- testQuizFolder/getTMA.py
+++ testQuizFolder/getTMA.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+import json
 import random
 
 
@@ -34,7 +34,8 @@ def get_module_questions(txtFileLoc:str()):
             quizQuestions[str(counter)] = questionObj
         print(quizQuestions[str(counter)])
         counter+=1
-
+    questions = json.dumps(quizQuestions)
+    return questions
 
 
 def get_questions():
@@ -53,3 +54,35 @@ def get_questions():
 
 if __name__ == '__main__':
     get_module_questions('test123')
+
+
+''' 12/06/2023
+def get_module_questions(txtFileLoc:str()):
+    questionLines = load_file('/home/kali/Documents/quizproject-main/testQuizFolder/module06.txt')
+    counter = 1
+    quizQuestions = dict()
+    for lines in questionLines:
+        lines.rstrip()
+        questionObject = lines.split('*')
+        questionObject[2] = questionObject[2].split('-')
+        questionObject[3] = questionObject[3].split('-')
+        if questionObject[0] =='M':
+            questionObject[3][-1] = questionObject[3][-1][0]
+            questionObj = {
+                "Type" : questionObject[0],
+                "Question": questionObject[1],
+                "Solutions": questionObject[2],
+                "correctAnswers": questionObject[3]}
+            quizQuestions[str(counter)] = questionObj
+        elif questionObject[0] =='F':
+            questionObject[3][-1] = questionObject[3][-1].rstrip()
+            questionObj = {
+                "Type" : questionObject[0],
+                "Question" : questionObject[1],
+                "Solutions" : questionObject[2],
+                "CorrectAnswers": questionObject[3]}
+            quizQuestions[str(counter)] = questionObj
+        print(quizQuestions[str(counter)])
+        counter+=1
+    return quizQuestions
+'''
